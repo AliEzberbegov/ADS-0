fac659f5ce11794c127be2d00151c2c5913c3e32
Ali <76096715+AliEzberbegov@users.noreply.github.com>
2021-02-25 09:37:44 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,20 +1,17 @@
 // Copyright 2021 NNTU-CS
 
-#include <iostream>
-#include "alg.h"
 int gcd(int a, int b) {
    a = abs(a);
-	b = abs(b);
-	while (b) {
-		while (a >= b)
-			a -= b;
-		if (!a)
-			return b;
-		do {
-			b -= a;
-		} while (b >= a);
+   b = abs(b);
+    while (b) {
+	while (a >= b)
+	a -= b;
+	if (!a)
+	return b;
+	do {
+	b -= a;
+	} while (b >= a);
     }
     return a;
 }
-int main() { std::cout << gcd(10,15) << std::endl; return 0; }
-main();
+
